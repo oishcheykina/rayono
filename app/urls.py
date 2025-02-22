b05af259ead2199428f470b7cdd8231cd839a596
BekhruzCo <109304010+BekhruzCo@users.noreply.github.com>
2025-02-22 23:15:27 +0500

    ass
    asas

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,11 @@ urlpatterns = [
     path('', home, name='home'),
     path('xalq-talimi-bolimi/xalq-talimi-bolimi/' , xalq_talimi_bolimi, name='xalq-talimi-bolimi'),
     path('oqituvchilarga/oqituvchilarga/', oqituvchilarga, name='oqituvchilarga'),
-    path('oqituvchilarga/dars-ishlanmalar/', dars_ishlanmalar, name='dars-ishlanmalar')
+    path('oqituvchilarga/dars-ishlanmalar/', dars_ishlanmalar, name='dars-ishlanmalar'),
+    path('xalq-talimi-bolimi/rahbariyat/' , rahbariyat),
+    path('xalq-talimi-bolimi/apparat-xodimlari/' , apparat_xodimlari ),
+    path('xalq-talimi-bolimi/tarkibiy-tuzilma/' , tarkibiy_tuzilma),
+    path('xalq-talimi-bolimi/rahbariyat-qabul-kunlari/' , rahbariyat_qabul_kunlari),
+    path('xalq-talimi-bolimi/bolim-nizomi/' , bolim_nizomi),
     
-
 ]
-
--- app/views.py
+++ app/views.py
@@ -5,8 +5,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
-
-
 def xalq_talimi_bolimi(request):
     return render(request , 'xalq-talimi-bolimi/xalq-talimi-bolimi.html')
 
@@ -15,4 +13,19 @@ def oqituvchilarga(request):
 
 def dars_ishlanmalar(request):
     return render(request,'oqituvchilarga/dars-ishlanmalar.html' )
+
+def rahbariyat(request):
+    return render(request, 'xalq-talimi-bolimi/rahbariyat.html')
+
+def apparat_xodimlari(request):
+    return render(request , 'xalq-talimi-bolimi/apparat-xodimlari.html')
+
+def tarkibiy_tuzilma(request):
+    return render(request , 'xalq-talimi-bolimi/tarkibiy-tuzilma.html')
+
+def rahbariyat_qabul_kunlari(request):
+    return render(request , 'xalq-talimi-bolimi/rahbariyat-qabul-kunlari.html')
+
+def bolim_nizomi(request):
+    return render(request , 'xalq-talimi-bolimi/bolim-nizomi.html')
 
