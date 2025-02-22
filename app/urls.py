2c4632f3afd0ff36f822d2abcbd6ff3abdf2b70d
Азиз <russr814@gmail.com>
2025-02-22 15:17:00 +0300

    обновил код

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import *
-
 urlpatterns = [
     path('', home, name='home'),
-    path('xalq-talimi-bolimi/xalq-talimi-bolimi/' , xalq_talimi_bolimi ),
-
+    path('xalq-talimi-bolimi/xalq-talimi-bolimi/' , xalq_talimi_bolimi, name='xalq-talimi-bolimi'),
+    path('oqituvchilarga/oqituvchilarga/', oqituvchilarga, name='oqituvchilarga'),
+    path('oqituvchilarga/dars-ishlanmalar/', dars_ishlanmalar, name='dars-ishlanmalar')
     
 
 ]
