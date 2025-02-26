88700e7e6a9716369373a101f9e2bf34a99a341c
BekhruzCo <109304010+BekhruzCo@users.noreply.github.com>
2025-02-26 19:47:48 +0500

    hh
    hh

--- app/views.py
+++ app/views.py
@@ -1,42 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
-from .models import *
+from django.shortcuts import render
 
 # MTM BO"LIMI
 def home(request):
-    boss = Boss.objects.first()
-    posts = Post.objects.all().order_by('-created_at')  
-    last_post = Post.objects.order_by('-created_at').first()
-    paginator = Paginator(posts, 4)
-    page_number = request.GET.get('page')  # Получаем номер страницы из GET-параметра
-    page_obj = paginator.get_page(page_number)
-    yil_dasturi = Yil_Dasturi.objects.first()
-    dic = {
-        'boss': boss,
-        'page_obj': page_obj,
-        'yil_dasturi': yil_dasturi,
-        'last_post': last_post,
-    }
-    return render(request, 'index.html', dic)
-
-def more(request, slug):
-    boss = Boss.objects.all()
-    post = get_object_or_404(Post, slug=slug)
-    viewed_news = request.session.get('viewed_news', [])
-
-    if slug not in viewed_news:
-        post.views += 1
-        post.save(update_fields=['views'])
-        viewed_news.append(slug)
-        request.session['viewed_news'] = viewed_news
-    yil_dasturi = Yil_Dasturi.objects.first()
-    dic = {
-        'boss': boss,
-        'post': post,
-        'yil_dasturi': yil_dasturi,
-    }
-    return render(request, 'more.html', dic)
-
+    return render(request, 'index.html')
 
 def xalq_talimi_bolimi(request):
     return render(request , 'xalq-talimi-bolimi/xalq-talimi-bolimi.html')
@@ -73,57 +39,10 @@ def bolim_manzili(request):
 
 
 # O'QITUVCHILARGA
+
 def oqituvchilarga(request):
     return render(request,'oqituvchilarga/oqituvchilarga.html' )
 
 def dars_ishlanmalar(request):
     return render(request,'oqituvchilarga/dars-ishlanmalar.html' )
 
-def fanlar_boyicha_testlar(request):
-     return render(request,'oqituvchilarga/fanlar-boyicha-testlar.html' )
-
-def davlat_dasturlari(request):
-     return render(request,'oqituvchilarga/davlat-dasturlari.html' )
-
-def kasaba_uyishmasi(request):
-     return render(request,'oqituvchilarga/kasaba-uyishmasi.html' )
-
-#MATBUOT-XIMATI
-
-def elonlar(request):
-     return render(request,'matbuot-xizmati/elonlar.html' )
-
-def bolim_ish_rejasi(request):
-     return render(request,'matbuot-xizmati/bolim-ish-rejasi.html' )
-
-def fotogalereya(request):
-     return render(request,'matbuot-xizmati/fotogalereya.html' )
-
-def maruzalar(request):
-     return render(request,'matbuot-xizmati/maruzalar.html' )
-
-def matbuot_xizmati(request):
-     return render(request,'matbuot-xizmati/matbuot-xizmati.html' )
-
-def tadbirlar_rejasi(request):
-     return render(request,'matbuot-xizmati/tadbirlar-rejasi.html' )
-
-def videogalereya(request):
-     return render(request,'matbuot-xizmati/videogalereya.html' )
-
-def yangiliklar(request):
-     return render(request,'matbuot-xizmati/yangiliklar.html' )
-
-#FAOLIYAT
-
-def besh_muhim_tashabbus(request):
-     return render(request,'faoliyat/besh-muhim-tashabbus.html' )
-
-def faoliyat(request):
-     return render(request,'faoliyat/faoliyat.html' )
-
-def korrupsiyaga_qarshi_kurash(request):
-     return render(request,'faoliyat/korrupsiyaga-qarshi-kurash.html' )
-
-def talimga_doir_terminlar(request):
-     return render(request,'faoliyat/talimga-doir-terminlar.html' )
